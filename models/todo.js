f1737f08af3fadd9902fc310b5ffec70a226fd0f
jonas olsson <johlson7@gmail.com>
2014-05-15 09:31:35 +0200

    delete shit

--- models/todo.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Todo = sequelize.define('Todo', {
-    value: DataTypes.STRING,
-    done: { type: DataTypes.BOOLEAN, defaultValue: false }
-  },{
-  timestamps: false
-  })
-  return Todo
-}
